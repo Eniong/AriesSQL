01a8fe3085b89eb13339a2bf9e9436d5e92b976a
Eniong <Eniong@github.com>
2019-03-23 21:13:45 +0800

    beta

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,37 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
+  },
+  {
+    "id": "cordova-plugin-sqlite-2.sqlitePlugin",
+    "file": "plugins/cordova-plugin-sqlite-2/dist/sqlite-plugin.js",
+    "pluginId": "cordova-plugin-sqlite-2",
+    "clobbers": [
+      "sqlitePlugin"
+    ]
+  },
+  {
+    "id": "cordova-sqlite-storage.SQLitePlugin",
+    "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+    "pluginId": "cordova-sqlite-storage",
+    "clobbers": [
+      "SQLitePlugin"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-device": "2.0.2",
+  "cordova-plugin-sqlite-2": "1.0.6",
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-sqlite-storage": "3.2.0"
+};
+// BOTTOM OF METADATA
+});
---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,43 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/src/browser/DeviceProxy.js",
+        "id": "cordova-plugin-device.DeviceProxy",
+        "pluginId": "cordova-plugin-device",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-sqlite-2/dist/sqlite-plugin.js",
+        "id": "cordova-plugin-sqlite-2.sqlitePlugin",
+        "pluginId": "cordova-plugin-sqlite-2",
+        "clobbers": [
+            "sqlitePlugin"
+        ]
+    },
+    {
+        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+        "id": "cordova-sqlite-storage.SQLitePlugin",
+        "pluginId": "cordova-sqlite-storage",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-device": "2.0.2",
+    "cordova-plugin-sqlite-2": "1.0.6",
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-sqlite-storage": "3.2.0"
+}
+// BOTTOM OF METADATA
+});
